7247926e26359b92ba51d8e92efefc098df810e9
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-12-28 00:17:18 +0530

    rename the paths to build app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
-import Loader from "@/components/Loader";
+import Loader from "../components/Loader";
 import { dark } from "@clerk/themes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Next Auth",
@@ -29,7 +21,7 @@ export default function RootLayout({ children }) {
     >
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={inter.className}
         >
           <ClerkLoading>
             <div className="flex justify-center items-center h-screen">
